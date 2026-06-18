7b7c4a93c9af6e1085c972c87ef80f99ec936467
rasoulmahjoobi <rasoulmahjoobi01@gmail.com>
2026-06-18 13:51:10 +0430

    navbar created in component-based

--- Web/script.js
+++ Web/script.js
@@ -1,7 +1,12 @@
-const response = await fetch(
-  "http://localhost:3000/products"
-);
+// const response = await fetch(
+//   "http://localhost:3000/products"
+// );
 
-const products = await response.json();
+// const products = await response.json();
 
-console.log(products);
+// console.log(products);
+
+
+// navbar-test
+import { navbar } from "./src/components/navbar.js";
+document.getElementById("navbar").innerHTML = navbar();
